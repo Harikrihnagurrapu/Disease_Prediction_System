161504b66cff0095bfdded8250fe2b16df546879
Harirkishna Gurrapu <harikrishnagurrapu112@gmail.com>
2025-01-31 19:56:01 +0530

    uff

--- Backend/app.py
+++ Backend/app.py
@@ -182,5 +182,8 @@ def predict_heart():
         return jsonify({'error': str(e), 'traceback': traceback.format_exc()}), 500
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Render assigns a port
+    app.run(host="0.0.0.0", port=port)
